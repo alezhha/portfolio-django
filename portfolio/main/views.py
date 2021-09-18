0a821295d1400a45446d93842329c76ef8925461
Alezhha <callermanjohnson@gmail.com>
2021-09-18 15:23:12 +0600

    made admin stuff

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from .models import SendMessage
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def formMessage(request):
+    if request.method == "POST":
+        sendMessage = SendMessage()
+        sendMessage.email = request.POST.get('email')
+        sendMessage.phone_number = request.POST.get('phone_number')
+        sendMessage.message = request.POST.get('message')
+        sendMessage.save()
+        return HttpResponseRedirect('/')
